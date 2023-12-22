bbd54c9cde6988cd4c9137df5031cb9748430da0
wendellhernandez <wendellhbusiness@gmail.com>
2023-12-22 09:21:42 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Template</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Macondo+Swash+Caps&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="assets/rock.png" type="image/x-icon">
+    <link rel="stylesheet" href="style.min.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <h1>Template</h1>
+        </div>
+    </header>
+
+    <main>
+        
+    </main>
+
+    <footer>
+        Project by: <a href="https://github.com/wendellhernandez">Wendell Hernandez</a>
+        <br>
+        Tech Stack: <span>HTML, CSS, Javascript, LESS</span>
+        <br><br>
+        Copyright &copy; 2023
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="app.js"></script>
+</body>
+</html>
