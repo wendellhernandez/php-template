a8d5c3dfe99a5d89256a9362f5ec4bb79511a827
wendellhernandez <wendellhbusiness@gmail.com>
2023-12-24 16:58:02 +0800

    Change template to index in index.php. Added height to scrollbar.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Template</title>
+    <title>Index</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Macondo+Swash+Caps&display=swap" rel="stylesheet">
@@ -14,7 +14,7 @@
 <body>
     <header>
         <div class="container">
-            <h1>Template</h1>
+            <h1>Index</h1>
         </div>
     </header>
 
